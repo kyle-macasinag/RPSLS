863213d51f4c83950038555d2cae1037e2dac70c
kyle.macasinag <kylenmac@verizon.net>
2022-04-06 12:07:34 -0400

    Set up framework for game rules

---
+++ players.py
@@ -0,0 +1,9 @@
+class Players():
+    #Display rules and matchups
+    #Choose players (ai or player 2)
+    #Each player selects a gesture
+    #Determine winner of round
+    #Display results
+    #Record results(If there was a winner, their win counter goes up)
+    #Play until win counter reaches 2
+    #Display winner of game
